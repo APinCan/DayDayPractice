9d5ab0be83acf0d29862ff208ba9b564f2ce1d84
APinCan <6794ch@naver.com>
2019-05-15 14:10:20 +0900

    tesseract 적용

--- app/src/main/java/com/example/navigatinodrawaertest/MemoActivity.java
+++ app/src/main/java/com/example/navigatinodrawaertest/MemoActivity.java
@@ -1,21 +1,32 @@
 package com.example.navigatinodrawaertest;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.InputStream;
+
 public class MemoActivity extends AppCompatActivity {
+    private final int PICK_FROM_ALBUM = 2000;
+    private final int PICK_FROM_ALBUM_TEXTRECOGNITION=2001;
+
     EditText editTextTitle;
     EditText editTextMain;
     TextView textViewAddress;
     MemoAdapter memoAdapter;
     Intent intent;
+    Bitmap inputImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +50,23 @@ public class MemoActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.memo_main_toolbar, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_insert_image:
+                goToAlbum();
+                return true;
+            case R.id.action_capture_image_convert:
+                gotToAlbumTextRecognition();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -50,5 +77,61 @@ public class MemoActivity extends AppCompatActivity {
 
         Log.d("ADAPTOR", "onBackPressed()");
         super.onBackPressed();
+    }
+
+    private void goToAlbum(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    private void gotToAlbumTextRecognition(){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(intent, PICK_FROM_ALBUM_TEXTRECOGNITION);
+    }
+
+    //http://www.masterqna.com/android/68879/edittext%EC%97%90-%EC%9D%B4%EB%AF%B8%EC%A7%80%EB%A5%BC-%EB%84%A3%EC%9D%84%EC%88%98%EC%9E%88%EB%82%98%EC%9A%94
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==PICK_FROM_ALBUM){
+            if(resultCode==RESULT_OK){
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+
+                    inputImage= BitmapFactory.decodeStream(in);
+                    in.close();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        else if(requestCode==PICK_FROM_ALBUM_TEXTRECOGNITION){
+            if(resultCode==RESULT_OK){
+                try {
+                    InputStream in  =getContentResolver().openInputStream(data.getData());
+
+                    inputImage=BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    textRecognition();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void textRecognition(){
+        TesseractOCR tesseractOCR = new TesseractOCR(this, "eng");
+        String text=tesseractOCR.getOCRResult(inputImage);
+
+        Toast.makeText(this, "recognizing....", Toast.LENGTH_SHORT).show();
+
+        editTextMain.setText(text);
     }
 }
---
+++ app/src/main/java/com/example/navigatinodrawaertest/TesseractOCR.java
@@ -0,0 +1,101 @@
+package com.example.navigatinodrawaertest;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import static android.support.constraint.Constraints.TAG;
+
+//https://www.codeproject.com/Articles/1275580/Android-OCR-Application-Based-on-Tesseract
+//https://bluebead38.blogspot.com/2017/06/android-studio-opencv-tessseract-ocr.html
+public class TesseractOCR {
+
+    private final TessBaseAPI mTess;
+
+    public TesseractOCR(Context context, String language) {
+        mTess = new TessBaseAPI();
+        boolean fileExistFlag = false;
+
+        //t넘겨준 인자 this에서 Asset을 가져오고
+        AssetManager assetManager = context.getAssets();
+
+        //tessdata가 있는 경로 설정정
+       String dstPathDir = "/tesseract/tessdata/";
+
+        String srcFile = "eng.traineddata";
+        InputStream inFile = null;
+
+        //getFilesDir()하면 data/data/패키지명/files의 경로 얻어옴
+        dstPathDir = context.getFilesDir() + dstPathDir;
+        String dstInitPathDir = context.getFilesDir() + "/tesseract";
+        //dstPathFile = data/datda/패키지명/files/tesseract/tessdata/eng.traineddata
+        String dstPathFile = dstPathDir + srcFile;
+        FileOutputStream outFile = null;
+
+        try {
+            inFile = assetManager.open(srcFile);
+
+            File f = new File(dstPathDir);
+
+            if (!f.exists()) {
+                if (!f.mkdirs()) {
+                    Toast.makeText(context, srcFile + " can't be created.", Toast.LENGTH_SHORT).show();
+                }
+                outFile = new FileOutputStream(new File(dstPathFile));
+            } else {
+                fileExistFlag = true;
+            }
+
+        } catch (Exception ex) {
+            Log.e(TAG, ex.getMessage());
+
+        } finally {
+
+            if (fileExistFlag) {
+                try {
+                    if (inFile != null) inFile.close();
+                    mTess.init(dstInitPathDir, language);
+                    return;
+
+                } catch (Exception ex) {
+                    Log.e(TAG, ex.getMessage());
+                }
+            }
+
+            if (inFile != null && outFile != null) {
+                try {
+                    //copy file
+                    byte[] buf = new byte[1024];
+                    int len;
+                    while ((len = inFile.read(buf)) != -1) {
+                        outFile.write(buf, 0, len);
+                    }
+                    inFile.close();
+                    outFile.close();
+                    mTess.init(dstInitPathDir, language);
+                } catch (Exception ex) {
+                    Log.e(TAG, ex.getMessage());
+                }
+            } else {
+                Toast.makeText(context, srcFile + " can't be read.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public String getOCRResult(Bitmap bitmap) {
+        mTess.setImage(bitmap);
+        return mTess.getUTF8Text();
+    }
+
+    public void onDestroy() {
+        if (mTess != null) mTess.end();
+    }
+}
